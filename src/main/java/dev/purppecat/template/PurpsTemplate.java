a702e80618ebecae04895b5270a20d69e03eec59
purppecat7890 <purpplecats@gmail.com>
2026-06-02 16:52:03 +0300

    main commit

--- src/main/java/dev/purppecat/template/PurpsTemplate.java
+++ src/main/java/dev/purppecat/template/PurpsTemplate.java
@@ -1,4 +1,4 @@
-package dev.thomasglasser.neoforgetemplate;
+package dev.purppecat.template;
 
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
@@ -6,13 +6,13 @@ import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(NeoForgeTemplate.MOD_ID)
-public class NeoForgeTemplate {
-    public static final String MOD_ID = "neoforgetemplate";
-    public static final String MOD_NAME = "NeoForge Template";
+@Mod(PurpsTemplate.MOD_ID)
+public class PurpsTemplate {
+    public static final String MOD_ID = "template";
+    public static final String MOD_NAME = "Purp's Template";
     private static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    public NeoForgeTemplate(IEventBus modEventBus) {}
+    public PurpsTemplate(IEventBus modEventBus) {}
 
     public static ResourceLocation modLoc(String path) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
